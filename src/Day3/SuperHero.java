e64e274e0bcaa3219566507ba645b16557bfc2eb
Ganesh-HA <haganesh2000@gmail.com>
2023-07-12 16:32:34 +0530

    Initial

--- src/Day3/SuperHero.java
+++ src/Day3/SuperHero.java
@@ -4,22 +4,40 @@ public class SuperHero {
     private String power;
     private String superpower;
 
-    public SuperHero(String name, String power, String superpower) {
+    /*public SuperHero(String name, String power, String superpower) {
         this.name = name;
         this.power = power;
         this.superpower = superpower;
-    }
+    }*/
     public String getName() {
         return name;
+    }
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getPower() {
         return power;
     }
+    public void setPower(String power) {
+        this.power = power;
+    }
 
     public String getSuperpower() {
+
         return superpower;
     }
 
+    public void setSuperpower(String superpower) {
+        this.superpower = superpower;
+    }
 
+    @Override
+    public String toString() {
+        return "SuperHero{" +
+                "name='" + name + '\'' +
+                ", power='" + power + '\'' +
+                ", SuperPower='" + superpower + '\'' +
+                '}';
+    }
 }
