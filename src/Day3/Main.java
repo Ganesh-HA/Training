24bb271a7eb05ebe8ddcead52635db4c9f38baf9
Ganesh-HA <haganesh2000@gmail.com>
2023-07-12 18:06:02 +0530

    Initial

--- src/Day3/Main.java
+++ src/Day3/Main.java
@@ -2,17 +2,18 @@ package Day3;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+
         Scanner scr = new Scanner(System.in);
         SuperHero s = new SuperHero();
         System.out.println("Enter the SuperHero's Name: ");
         String name = scr.nextLine();
-        //System.out.println("Enter the SuperHero's power: ");
-        //String power = scr.nextLine();
-        //System.out.println("Enter the SuperHero's SuperPower: ");
-        //String superpower = scr.nextLine();
+        System.out.println("Enter the SuperHero's power: ");
+        String power = scr.nextLine();
+        System.out.println("Enter the SuperHero's SuperPower: ");
+        String superpower = scr.nextLine();
         s.setName(name);
-        s.setPower("Intelligence,Strength");
-        s.setSuperpower("IronMan Suit");
+        s.setPower(power);
+        s.setSuperpower(superpower);
         System.out.println(s);
 
 
